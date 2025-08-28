bb5fd571c18b5eacaebd65332c28e48bf1af3957
kalachuuchiiii <parissrowlet@gmail.com>
2025-08-28 23:10:04 +0800

    defined config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
+import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, 
-    port: 5173
-  }
-})
+    host: true, // for dev mode
+  },
+  preview: {
+    host: true, // bind to 0.0.0.0 in preview
+    port: process.env.PORT || 5173,
+    allowedHosts: ["inquestia.up.railway.app"],
+  },
+});
+

91fd2a2b4df288c6c846f88f76aecd4bd435c642
kalachuuchiiii <parissrowlet@gmail.com>
2025-08-28 21:11:49 +0800

    aa

--- server/server.js
+++ server/server.js
@@ -3,12 +3,12 @@ if(process.env.NODE_ENV !== "production"){
 }
 const express = require("express");
 const cors = require("cors");
-const app = express();
 const redis = require("./src/config/redis/index.js");
 const cloudinary = require("./src/config/cloudinary/index.js");
 const { connectDB } = require("./src/config/mongodb/index.js");
 const mainRouter = require("./src/router/index.js");
 const serverless = require("serverless-http");
+const app = express();
 
 app.use(express.json());
 app.use(cors({

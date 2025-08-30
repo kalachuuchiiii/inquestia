4086b7e41e7c7cbb7ffb86b8ef9b229a55798282
kalachuuchiiii <parissrowlet@gmail.com>
2025-08-30 12:15:39 +0800

    basename: '/'

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import Main from './layout/main.jsx';
 import { Provider } from 'react-redux';
@@ -8,9 +8,9 @@ import store from './state/store.js';
 
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter basename = "/">
     <Provider store = {store} >
       <Main />
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 )

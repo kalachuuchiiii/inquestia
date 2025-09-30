2abad2d2524093ab965f9d9f6adbddb3d0067766
kalachuuchiiii <parissrowlet@gmail.com>
2025-09-30 22:32:02 +0800

    fixed nodemailer

--- server/src/utils/email/sendEmail.js
+++ server/src/utils/email/sendEmail.js
@@ -1,23 +1,12 @@
 const transporter = require("../../config/nodemailer/index.js");
 
-
-// utils/sendEmail.js
 const nodemailer = require("nodemailer");
 
 exports.sendEmail = async ({ to, subject, html }) => {
   try {
-    // Create reusable transporter
-    const transporter = nodemailer.createTransport({
-      service: "gmail", // or 'hotmail', 'yahoo', or custom SMTP
-      auth: {
-        user: "parissrowlet@gmail.com",
-        pass: process.env.APP_KEY, 
-      },
-    });
-
-    // Send email
+   
     const info = await transporter.sendMail({
-      from: `parissrowlet@gmail.com`, // sender address
+      from: `parissrowlet@gmail.com`, 
       to,
       subject,
       html,

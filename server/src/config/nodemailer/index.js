1828e94db51f648e6b703da424078545caa3e53a
kalachuuchiiii <parissrowlet@gmail.com>
2025-10-01 07:52:18 +0800

    added transporter verification

--- server/src/config/nodemailer/index.js
+++ server/src/config/nodemailer/index.js
@@ -1,11 +1,22 @@
+
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  service: 'gmail',
   auth: {
-    user: "parissrowlet@gmail.com", 
+    user: "parissrowlet@gmail.com",
     pass: process.env.APP_KEY
-  }
-})
+  },
+  tls: {
+    rejectUnauthorized: false
+  },
+  connectionTimeout: 20000
+});
+
+transporter.verify((error, success) => {
+  if (error) console.log("SMTP error:", error);
+  else console.log("SMTP ready:", success);
+});
 
 module.exports = transporter;

9ea1ff1bfdb1a86c153a97f5a61cd5524ba3082c
kalachuuchiiii <parissrowlet@gmail.com>
2025-10-01 09:33:49 +0800

    sssdf

--- server/src/config/nodemailer/index.js
+++ server/src/config/nodemailer/index.js
@@ -1,11 +1,16 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail', 
+  host: "smtp.gmail.com",
+  secure: true,
   auth: {
-    user: "parissrowlet@gmail.com", 
-    pass: process.env.APP_KEY
-  }
-})
+    user: "parissrowlet@gmail.com",
+    pass: process.env.APP_KEY,
+  },
+  rejectUnauthorized: false,
+  tls: {
+    ciphers: "SSLv3",
+  },
+});
 
 module.exports = transporter;
--- server/src/utils/email/sendEmail.js
+++ server/src/utils/email/sendEmail.js
@@ -1,8 +1,20 @@
-const transporter = require("../../config/nodemailer/index.js");
+const nodemailer = require('nodemailer');
 
 exports.sendEmail = async ({ to, subject, html }) => {
   try {
     console.log(to, subject);
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      secure: true,
+      auth: {
+        user: "parissrowlet@gmail.com",
+        pass: process.env.APP_KEY,
+      },
+      rejectUnauthorized: false,
+      tls: {
+        ciphers: "SSLv3",
+      },
+    });
    
     const inf = await new Promise((resolve, reject) => {
     transporter.sendMail(

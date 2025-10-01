5b607a5f49137adb94f33b4d125196780118069c
kalachuuchiiii <parissrowlet@gmail.com>
2025-10-01 09:25:54 +0800

    trying solutions

--- server/src/utils/email/sendEmail.js
+++ server/src/utils/email/sendEmail.js
@@ -4,15 +4,26 @@ exports.sendEmail = async ({ to, subject, html }) => {
   try {
     console.log(to, subject);
    
-    const info = await transporter.sendMail({
-      from: `parissrowlet@gmail.com`, 
-      to,
-      subject,
-      html,
-    });
-    console.log(info)
+    const inf = await new Promise((resolve, reject) => {
+    transporter.sendMail(
+      {
+        from: `parissrowlet@gmail.com`,
+        to,
+        subject,
+        html,
+      },
+      (err, info) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(info);
+        }
+      }
+    );
+  });
 
-    return { success: true, data: info };
+    return { success: true, data: inf };
   } catch (error) {
     console.log(error)
     throw error;

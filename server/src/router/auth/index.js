1db494caee0a74ab05cfbcd607aaa5f495e81105
kalachuuchiiii <parissrowlet@gmail.com>
2025-10-01 10:20:25 +0800

    removed another dirreader

--- server/src/router/auth/index.js
+++ server/src/router/auth/index.js
@@ -1,8 +1,8 @@
-const { readOneFile } = require("../../utils/directoryReader.js");
+
 const coreRouter = require("express").Router();
 
-const userRouter = readOneFile([__dirname, "../..", "controllers", "core", "auth", "index.js"]);  
-const adminRouter = readOneFile([__dirname, "../..", "controllers", "core", "admin", "index.js"]);  
+const userRouter = require("../../controllers/core/auth/index.js");
+const adminRouter = require("../../controllers/core/admin/index.js");
 
 coreRouter.use(userRouter);
 coreRouter.use(adminRouter);

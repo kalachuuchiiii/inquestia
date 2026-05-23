208e1c34c4b2361345510f48f81d53e45286176d
kalachuuchiiii <parissrowlet@gmail.com>
2026-05-23 20:15:57 +0800

    set up package settings,add portbinding 0.0.0.0

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ["inquestia.up.railway.app"],
+  },
   server: {
     hmr: {
-      overlay: true
-    }
+      overlay: true,
+    },
   },
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, './src'),
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
